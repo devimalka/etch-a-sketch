9a0912e8299465ed4413e5e09d80d61ca3e681c8
devim <developerimalka@gmail.com>
2021-03-08 17:52:02 +0530

    initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,24 @@
 function fillgrid(size){
     const container = document.querySelector('.container');
     container.setAttribute('style',`grid-template-columns:repeat(${size},1fr);grid-template-rows:repeat(${size},1fr)`);
-    for(let i=0;i<size*size;i++){
+    for(let i=1;i<=size*size;i++){
         let div = document.createElement('div');
-        div.textContent = i;
+        //div.textContent = i;
         div.classList.add('cell');
         container.appendChild(div);
 
     }
 }
+
+function clearGrids(){
+    let container = document.querySelector('.container');
+    let grids = document.querySelectorAll('.cell');
+    for(let i=0;i<grids.length;i++){
+        container.removeChild(grids[i]);
+    }
+}
+
+
 
 function randomColor(){
     let letters = '0123456789ABCDEF';
@@ -19,18 +29,27 @@ function randomColor(){
     return color;
 }
 
-
-const divs = document.querySelectorAll('.cell');
-
-divs.forEach((div) => {
-    div.addEventListener('mouseover',() =>{
-        div.style.backgroundColor = randomColor();
-    })
-})
+fillgrid(16);
 
 let button = document.querySelector("#size");
 
 button.addEventListener('click',()=>{
     let size = parseInt(prompt("enter size"));
+    clearGrids();
     fillgrid(size);
+    fillcolor();
 })
+
+
+function fillcolor(){
+let test = document.querySelectorAll('.cell');
+
+test.forEach((div) => {
+    div.addEventListener('mouseover',() =>{
+        div.style.backgroundColor = randomColor();
+    })
+})
+
+}
+
+fillcolor();
